b2ed6e3e1ab43176a6aa37d69bf8809a6f704cee
Beatriz <bbiasantoscorreia@gmail.com>
2021-08-27 11:45:46 -0300

    primeira parte do desafio

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function clickDark(){
+        if(document.body.style.backgroundColor == "white"){
+
+            document.body.style.backgroundColor = "black";
+            document.body.style.color = "white";
+        }
+        else{
+            document.body.style.backgroundColor = "white";
+            document.body.style.color = "black";
+        }
+    }
